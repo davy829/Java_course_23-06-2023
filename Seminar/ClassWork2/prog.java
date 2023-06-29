887ede8feea24334f71829b4d0ec6b22b9f62277
Andrey <daandrey485@gmail.com>
2023-06-29 16:40:02 +0300

    add to file

--- Seminar/ClassWork2/prog.java
+++ Seminar/ClassWork2/prog.java
@@ -1,8 +1,16 @@
 package Seminar.ClassWork2;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.lang.StringBuilder;
 public class prog {
     public static void main(String[] args) {
       //System.out.println(  strLenBuild(10, '0', 'g'));
+      //System.out.println(polindrom("asdo5dsa"));
+      System.out.println(makeString("test", 5));
+      String pathPrj = System.getProperty("user.dir");
+      String pathFile = pathPrj.concat("\\tester2.txt");
+      addToFile(pathFile,makeString("pathFile", 10));
     }
 
     static String strLenBuild(int n, char c1,char c2){
@@ -14,12 +22,38 @@ public class prog {
         return mystr.toString();
     }
 
-    //Напишите метод, который принимает на вход строку (String) и определяет является
-    // ли строка палиндромом (возвращает boolean значение).
-   // static String polindrom(String s){
-   //   StringBuilder mystr = new StringBuilder(s);
-        
-    
-   // }
+ 
+    static String polindrom(String s){
+       //Напишите метод, который принимает на вход строку (String) и определяет является
+    // ли строка палиндромом (возвращает boolean значение).    
+       for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) != s.charAt(s.length() - i-1))
+         return s + " No polindrom";      
+       }
+       return s +" yes plindrom" ;
+    }
 
+ static String makeString(String s, int lenStr){
+   StringBuilder sb = new StringBuilder();
+  for (int i = 0; i < lenStr; i++) {
+    sb.append(i);
+    sb.append(") "+ s + "\n");
+  
+  }
+  return sb.toString();
+
+ }
+
+static void addToFile(String fileName, String msg2){
+try {
+    File myFile = new File(fileName);
+    FileWriter writer = new FileWriter(myFile);
+    writer.write(msg2);
+    writer.close();
+} catch (Exception e) {
+    System.out.println(e.getMessage());
+}
+
+}
+ 
 }
---
+++ Seminar/HomeWork3/program.java
@@ -0,0 +1,41 @@
+package Seminar.HomeWork3;
+public class program {
+    public static void main(String[] args) {
+    prost(20);
+    }
+
+    static void prost(int n){
+        StringBuilder sb = new StringBuilder();
+      /*  if (n > 2 & n % 2 != 0){
+        for (int i = 3; i < n / 2; i = i++) {
+        }
+        if (i % 2 != 0){
+              System.out.printf("n =%s \n",i);   
+        }
+*/
+           
+           
+           // sb.append(i);
+            //sb.append(",");
+           // System.out.println(i);
+          
+            
+ 
+ 
+
+   /*  static int rostoe(int n1){
+    if (n1 > 2 & n1 % 2 != 0){
+        for (int i = 3; i < (n1 / 2); i++) {
+          if (n1 % 2 != 0){
+         return i;
+          }  
+        }
+*/
+            
+     
+      
+
+   
+
+  }
+  }
